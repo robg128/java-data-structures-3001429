bbb00e22639e1eac11d72888bb24f6dd7c1c1c60
Rob Glasener <robglasener@me.com>
2021-12-27 16:08:37 -0800

    re worked arrays

--- src/main/java/ArrayExamples.java
+++ src/main/java/ArrayExamples.java
@@ -2,26 +2,26 @@ import java.util.Arrays;
 
 public class ArrayExamples {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    String[] weekdays = new String[7];
-    System.out.println(Arrays.toString(weekdays));
+        String[] weekdays = new String[7];
+        System.out.println(Arrays.toString(weekdays));
 
-    weekdays[0] = "Monday";
-    System.out.println(Arrays.toString(weekdays));
+        weekdays = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+        System.out.println(Arrays.toString(weekdays));
 
-    weekdays = new String[]{"Mon", "Tue", "Wed"};
-    System.out.println(Arrays.toString(weekdays));
+        for (String day : weekdays
+        ) {
+            System.out.println("Week day: " + day);
 
-    for(String day : weekdays) {
-      System.out.println("Week day: " + day);
+        }
+
+        for (int i = 0; i < weekdays.length; i++) {
+            weekdays[i] = weekdays[i].toUpperCase();
+        }
+        System.out.println(Arrays.toString(weekdays));
+
+
     }
-
-    for(int i = 0; i < weekdays.length; i++) {
-      weekdays[i] = weekdays[i].toUpperCase();
-    }
-    System.out.println(Arrays.toString(weekdays));
-
-  }
 
 }
